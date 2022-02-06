80573a3551347b59f7c5e368426be4a2f8d4bd48
Akshayan <thiruakshayan98@gmail.com>
2022-02-06 16:53:00 +0530

    Handling Invalid Requests

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -14,10 +14,9 @@ namespace Repository
 
         public IEnumerable<Company> GetAllCompanies(bool trackChanges)
         {
-           return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+            return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
         }
-        public Company GetCompany(Guid companyId, bool trackChanges) =>
-            FindByCondition(c => c.Id.Equals(companyId), trackChanges)
-            .SingleOrDefault();
+        public Company GetCompany(Guid companyId, bool trackChanges) => FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+.SingleOrDefault();
     }
 }
--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 
@@ -29,7 +30,12 @@ namespace Service
         public CompanyDTO GetCompany(Guid id, bool trackChanges)
         {
             var company = _repository.Company.GetCompany(id, trackChanges); //Check if the company is null
-            var companyDTO = _mapper.Map<CompanyDTO>(company); return companyDTO; }
+            if (company is null)
+            {
+                throw new CompanyNotFoundException(id);
+            }
+            var companyDTO = _mapper.Map<CompanyDTO>(company); 
+            return companyDTO; }
 
         }
 }
