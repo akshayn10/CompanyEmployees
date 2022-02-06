b7a877506e02450cd6d7ed9fd8d6e6fefeb65918
Akshayan <thiruakshayan98@gmail.com>
2022-02-06 11:02:30 +0530

    Get Method Implemented In Controller

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -11,5 +11,10 @@ namespace Repository
     public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     {
         public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+           return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
         }
+    }
 }
