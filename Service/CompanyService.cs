3028761fff79ab507bfdbb3a96693a319e412964
Akshayan <thiruakshayan98@gmail.com>
2022-02-06 12:53:47 +0530

    Addded DTO and AutoMapper

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,6 +1,7 @@
-﻿using Contracts;
-using Entities.Models;
+﻿using AutoMapper;
+using Contracts;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service
 {
@@ -8,19 +9,22 @@ namespace Service
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
 
-        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;
         }
 
-            public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+            public IEnumerable<CompanyDTO> GetAllCompanies(bool trackChanges)
             {
                 try
                 {
                     var companies = _repository.Company.GetAllCompanies(trackChanges);
-                    return companies;
+                var companiesDTO = _mapper.Map<IEnumerable<CompanyDTO>>(companies);
+                return companiesDTO;
                 }
                 catch (Exception ex)
                 {
@@ -28,5 +32,6 @@ namespace Service
                     throw;
                 }
             }
+
     }
 }
